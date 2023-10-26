0becf91e4eff6d20edafcc5ac96ed79b58495bbe
Carlston Rebelo <130921730+Carlston325@users.noreply.github.com>
2023-10-26 12:38:10 +0100

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-var numberOfButtons=document.querySelectorAll(".btn").length;for(var i=0;i<numberOfButtons;i++){document.querySelectorAll(".btn")[i].addEventListener("mouseenter",function(){var buttonValue=(this.classList[1]);buttonHightlight(buttonValue)})}
-function buttonHightlight(btnKey){var activeButton=document.querySelector("."+btnKey);activeButton.classList.add("btnHighlight");for(var i=0;i<numberOfButtons;i++){document.querySelectorAll(".btn")[i].addEventListener("mouseleave",function(){var activeButton=document.querySelector("."+btnKey);activeButton.classList.remove("btnHighlight")})}}
+var numberOfButtons=document.querySelectorAll(".btn").length;
+
+for(var i=0; i<numberOfButtons; i++){
+    document.querySelectorAll(".btn")[i].addEventListener("mouseenter", function(){
+        var buttonValue=(this.classList[1]);
+        buttonHightlight(buttonValue);
+    })
+}
+
+function buttonHightlight(btnKey){
+    var activeButton=document.querySelector("." + btnKey);
+    activeButton.classList.add("btnHighlight");
+    
+    for(var i=0; i<numberOfButtons; i++){
+        document.querySelectorAll(".btn")[i].addEventListener("mouseleave", function(){
+            var activeButton=document.querySelector("."+btnKey);
+            activeButton.classList.remove("btnHighlight");
+        });
+    }
+}
